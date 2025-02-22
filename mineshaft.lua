db8c3b9ef8779e9ee99c749ec2a99915470cd2e4
Snachmo <pablo.elvira@gmail.com>
2025-02-22 13:36:55 -0800

    saturday morning fever

--- mineshaft.lua
+++ mineshaft.lua
@@ -1,27 +1,19 @@
--- Position (coordinates relative to starting position).
-position = { x = 0, y = 0, z = 0 }
--- Directions: 0 = north, 1 = east, 2 = south, 3 = west, 4 = up, -1 = down
-direction = 0
+ABUNDANT_BLOCKS = {
+  ["minecraft:dirt"] = true,
+  ["minecraft:grass_block"] = true,
+  ["minecraft:stone"] = true,
+  ["minecraft:deepslate"] = true,
+  ["minecraft:granite"] = true,
+}
 
-function turnRight()
-  turtle.turnRight()
-  direction = (direction + 1) % 4
-end
-
--- turnTo: Rotates the turtle to face an absolute direction (0 = north, etc.).
-function turnTo(newDirection)
-  if newDirection > -1 and newDirection < 4 then
-    local diff = (newDirection - direction) % 4
-    if diff == 3 then
-      turtle.turnLeft()
-      direction = (direction - 1) % 4
-    elseif diff == 2 then
-      turnRight(); turnRight()
-    elseif diff == 1 then
-      turnRight()
-    end
-  end
-end
+INDESTRUCTIBLE_BLOCKS = {
+  ["minecraft:bedrock"] = true,
+  ["minecraft:chest"] = true,
+  ["minecraft:ancient_debris"] = true,
+  ["minecraft:nether_portal"] = true,
+  ["minecraft:ender_portal"] = true,
+  ["minecraft:respawn_anchor"] = true,
+}
 
 HAZARD_BLOCKS = {
   ["minecraft:water"] = true,
@@ -36,47 +28,23 @@ HAZARD_BLOCKS = {
   ["minecraft:tnt"] = true,
 }
 
-function isHazard(block_name)
-  if HAZARD_BLOCKS[block_name] then
-    print("HAZARD: " .. block_name .. " detected.")
-    return true
+function isHazard(block)
+  if block != nil then
+    return HAZARD_BLOCKS[block]
   end
   return false
 end
 
-INDESTRUCTIBLE_BLOCKS = {
-    ["minecraft:bedrock"] = true,
-    ["minecraft:chest"] = true,
-    ["minecraft:ancient_debris"] = true,
-    ["minecraft:nether_portal"] = true,
-    ["minecraft:ender_portal"] = true,
-    ["minecraft:respawn_anchor"] = true,
-  }
-
-function isIndestructible(block_name)
-  if INDESTRUCTIBLE_BLOCKS[block_name] then
-    print("INDESTRUCTIBLE: " .. block_name .. " detected.")
-    return true
+function isDiggable(block, digAbundant)
+  if block != nil then
+    return not INDESTRUCTIBLE_BLOCKS[block] and not (ABUNDANT_BLOCKS[block] and digAbundant)
   end
   return false
 end
 
-ABUNDANT_BLOCKS = {
-  ["minecraft:dirt"] = true,
-  ["minecraft:grass_block"] = true,
-  ["minecraft:stone"] = true,
-  ["minecraft:deepslate"] = true,
-  ["minecraft:granite"] = true,
-  -- Add additional block types as desired.
-}
-
-function isAbundant(block_name)
-  return ABUNDANT_BLOCKS[block_name]
-end
-
-function getBlock(detectFunc, inspectFunc)
+function _getBlock(detectFunc, inspectFunc)
   if not detectFunc() then
-    print("getBlock.detectFunc() returned false")
+    print("getBlock found no block")
     return nil
   end
   local success, data = inspectFunc()
@@ -85,62 +53,119 @@ function getBlock(detectFunc, inspectFunc)
       print("getBlock.inspectFunc() successfully returned a nil block")
       return nil
     end
-    local block_name = data.name
-    return {
-      success = true,
-      error = nil,
-      name = block_name,
-      hazard = isHazard(block_name),
-      indestructible = isIndestructible(block_name),
-      abundant = isAbundant(block_name),
-    }
+    return data.name
   else
     print("CRITICAL: getBlock.inspectFunct() retured error " .. data)
-    return {
-      success = false,
-      error = data
-    }
+    return false
   end
 end
 
-function _safeDig(detectFunc, inspectFunc, digFunc, digAbundant)
-  local block = getBlock(detectFunc, inspectFunc)
-  if block == nil then
-    return nil
-  elseif block.abundant and digAbundant == false then
-    print("Not digging abundant block " .. block.name)
-    return false
-  elseif block.indestructible then
-    print("Not digging indestructible block " .. block.name)
-    return false
-  elseif not digFunc() then
-    print("CRITICAL: safeDig.digFunc() returned false.")
-    return false
-  end
-  return true
+-- Adjacent blocks
+front = nil
+up = nil
+down = nil
+
+function getFront()
+  front = _getBlock(turtle.detect, turtle.inspect)
+  return front
 end
 
-function safeDig(digDirection, digAbundant)
-  -- print("Attempting safeDig " .. digDirection)
-  if digDirection == 0 then
-    return _safeDig(turtle.detect, turtle.inspect, turtle.dig, digAbundant)
-  elseif digDirection == -1 then
-    return _safeDig(turtle.detectDown, turtle.inspectDown, turtle.digDown, digAbundant)
-  elseif digDirection == 4 then
-    return _safeDig(turtle.digUp, turtle.inspectUp, turtle.digUp, digAbundant)
+function getUp()
+  up = _getBlock(turtle.detectUp, turtle.inspectUp)
+  return up
+end
+
+function getDown()
+  down = _getBlock(turtle.detectDown, turtle.inspectDown)
+  return down
+end
+
+function inDanger()
+  getFront()
+  getUp()
+  getDown()
+  return isHazard(front) or isHazard(up) or isHazard(down)
+end
+
+function _dig(block, digFunc, digAbundant)
+  if block != nil and not INDESTRUCTIBLE_BLOCKS[block] and not (ABUNDANT_BLOCKS[block] and digAbundant) then
+    return digFunc()
+  end
+  return false
+end
+
+function digFront(digAbundant)
+  return _dig(getFront(), turtle.dig, digAbundant)
+end
+
+function digUp(digAbundant)
+  return _dig(getUp(), turtle.digUp, digAbundant)
+end
+
+function digDown(digAbundant)
+  return _dig(getDown(), turtle.digDown, digAbundant)
+end
+
+-- Position (coordinates relative to starting position).
+position = { x = 0, y = 0, z = 0 }
+
+-- Directions: 0 = north, 1 = east, 2 = south, 3 = west, 4 = up, -1 = down
+direction = 0
+
+-- Turn: -1 = left, 1 = right
+function turn(turnDirection)
+  if turnDirection == 1
+    turtle.turnRight()
+  elseif turnDirection == -1
+    turtle.turnLeft()
   else
-    print("CRITICAL: Bad safeDig.digDirection " .. digDirection)
     return false
   end
+  direction = (direction + turnDirection) % 4
+  getFront()
 end
 
-function goHomeFuel()
-  local buffer = 300
-  return turtle.getFuelLevel() - buffer - math.abs(position.x) - math.abs(position.y) - math.abs(position.z)
+function _move(moveFunc)
+  if moveFunc() then
+    if moveFunc == turtle.forward then
+      if direction == 0 then
+        position.z = position.z - 1
+      elseif direction == 1 then
+        position.x = position.x + 1
+      elseif direction == 2 then
+        position.z = position.z + 1
+      elseif direction == 3 then
+        position.x = position.x - 1
+      end
+    elseif moveFunc == turtle.up then
+      position.y = position.y + 1
+    elseif moveFunc == turtle.down then
+      position.y = position.y - 1
+    else
+      return false
+    end
+    getFront()
+    getUp()
+    getDown()
+    return true
+  end
+  return false
 end
 
-function refuelFromInventory()
-  print("Attempting to refuel from inventory with " .. turtle.getFuelLevel() .. " fuel remaining.")
+function move()
+  return _move(turtle.forward)
+end
+
+function moveUp()
+  return _move(turtle.up)
+end
+
+function moveDown()
+  return _move(turtle.down)
+end
+
+function refuel()
+  print("Attempting to refuel from inventory.")
   for slot = 1, 16 do
     turtle.select(slot)
     if turtle.refuel(0) then
@@ -154,120 +179,61 @@ function refuelFromInventory()
   return false
 end
 
-function checkFuelLevel()
-  print("Checking fuel level.")
-  while goHomeFuel() <= 0 do
-    if not refuelFromInventory() then
-      print("Failed to refuel.")
-      return false
-    end
-  end
-  return true
-end
--- abdsf
-
-
-function _safeMove(digDirection, moveFunc)
-  local blockUp = getBlock(turtle.detectUp, turtle.inspectUp)
-  local blockFront = getBlock(turtle.detect, turtle.inspect)
-  local blockDown = getBlock(turtle.detectDown, turtle.inspectDown)
-  if blockUp.hazard or blockForward.hazard or blockDown.hazard then
-    print("_safeMove() detected a hazardous block.")
-    return false
-  end
-  while not moveFunc() do
-    print("_safeMove(moveFunc()) returned false")
-    if not checkFuelLevel() then
-      print("_safeMove() failed: checkFuelLevel() returned false.")
-      return false
-    end
-    if not safeDig(digDirection, true) then
-      print("_safeMove() failed: safeDig() returned false.")
+fuel_buffer = 300
+function hasFuel()
+  local min_fuel = fuel_buffer + math.abs(position.x) + math.abs(position.y) + math.abs(position.z)
+  while turtle.getFuelLevel() <= min_fuel_level then
+    print("Low fuel. " .. fuel_level .. " remaining of " .. min_fuel_level .. " minimum.")
+    if not refuel() then
       return false
     end
   end
   return true
 end
 
-function safeMove(moveDirection)
-  print("Attempting safeMove " .. moveDirection)
-  if moveDirection > -1 and moveDirection < 4 then
-    turnTo(moveDirection)
-    return _safeMove(0, turtle.forward)
-  elseif moveDirection == -1 then
-    return _safeMove(moveDirection, turtle.down)
-  elseif moveDirection == 4 then
-    return _safeMove(moveDirection, turtle.up)
-  else
-    print("CRITICAL: Bad safeMove.moveDirection " .. moveDirection)
-    return false
-  end
-end
-  
-function moveTo(targetX, targetY, targetZ)
-  -- Vertical down move (minus Y axis).
-  while position.y > targetY do
-    if safeMove(-1, true) then
-      position.y = position.y - 1
+function safeMove(moveFunc, digFunc)
+  while hasFuel() and not inDanger() and not moveFunc() do
+      if not digFunc(true) then
+        return false
+      end
     end
   end
-  -- Move North (minus Z axis)
-  while position.z > targetZ do
-    if not safeMove(0, true) then
-      return false
-    end
-    position.z = position.z - 1
-  end
-  -- Move South (plus Z axis)
-  while position.z < targetZ do
-    if not safeMove(2, true) then
-      return false
-    end
-    position.z = position.z + 1
-  end
-  -- Move East (plus X axis)
-  while position.x < targetX do
-    if not safeMove(1, true) then
-      return false
-    end
-    position.x = position.x + 1
-  end
-  -- Move West (minus X axis)
-  while position.x > targetX do
-    if not safeMove(3, true) then
-      return false
-    end
-    position.x = position.x - 1
-  end
-  -- Vertical Up move (plus Y axis).
-  while position.y < targetY do
-    if not safeMove(4, true) then
-      return false
-    end
-    position.y = position.y + 1
-  end
-  return true
+  return false
 end
 
-function mineShaft(minY)
+function mineShaft(maxDepth)
   print("Starting fuel " .. turtle.getFuelLevel())
-  while minY < position.y do
+
+  while position.y > maxDepth do
     print("Digging down.")
-    if not moveTo(position.x, position.y-1, position.z) then
-      print("Failed to dig down, going home.")
-      moveTo(position.x, 0, position.z)
+    if not safeMove(moveDown, digDown) then
       return false
     end
-    print('Digging direction 0')
-    safeDig(0, false)
-    for turns = 1, 3 do
-      print('Digging direction ' .. turns)
-      turnRight()
-      safeDig(0, false)
+    digFront(false)
+    if inDanger() then
+      return false
+    end
+    for turn in 0, 2 do
+      turnLeft()
+      if inDanger() then
+        return false
+      end
+      digFront(false)
+      if inDanger() then
+        return false
+      end
     end
   end
-  print("Going home.")
-  moveTo(position.x, 0, position.z)
 end
 
-mineShaft(-10)
+function goHome()
+  print ("Going home.")
+  while position.y < 0 do
+    if not moveUp() then
+      digUp(true)
+    end
+  end
+end
+
+mineShaft(-40)
+goHome()
