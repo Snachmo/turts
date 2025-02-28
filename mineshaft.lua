e17ada0a8a6976ff96718dcea74bab3bf644a41b
Snachmo <pablo.elvira@gmail.com>
2025-02-27 22:19:15 -0800

    .

--- mineshaft.lua
+++ mineshaft.lua
@@ -1,9 +1,53 @@
+function inventoryFull()
+  for slot = 1, 16 do
+    if turtle.getItemCount(slot) == 0 then
+      return false
+    end
+  end
+  return true
+end
+
+function dropInventory()
+  print("Dropping inventory to front...")
+  for slot = 1, 16 do
+    turtle.select(slot)
+    if turtle.getItemCount() > 0 and not turtle.refuel(0) then
+        while not turtle.drop() do
+          print("Faild to drop item in slot " .. slot)
+        end
+      end
+  end
+  print('Dropped all non-fuel inventory.')
+  return true
+end
+
+function plugShaft()
+  while true do
+    for slot = 1, 16 do
+      turtle.select(slot)
+      if turtle.getItemCount() > 0 then
+        item = turtle.getItemDetail()
+        if item then
+          if item.name == "minecraft:obsidian" or item.name == "minecraft:cobblestone" then
+            turtle.placeDown()
+            return true
+          end
+        end
+      end
+    end
+    print("Failed to plug shaft!")
+  end
+end
+
 ABUNDANT_BLOCKS = {
   ["minecraft:dirt"] = true,
   ["minecraft:grass_block"] = true,
+  ["minecraft:gravel"] = true,
   ["minecraft:stone"] = true,
   ["minecraft:deepslate"] = true,
   ["minecraft:granite"] = true,
+  ["minecraft:diorite"] = true,
+  ["minecraft:andesite"] = true,
 }
 
 INDESTRUCTIBLE_BLOCKS = {
@@ -16,31 +60,8 @@ INDESTRUCTIBLE_BLOCKS = {
 }
 
 HAZARD_BLOCKS = {
-  ["minecraft:water"] = true,
-  ["minecraft:flowing_water"] = true,
-  ["minecraft:lava"] = true,
-  ["minecraft:flowing_lava"] = true,
-  ["minecraft:fire"] = true,
-  ["minecraft:soul_fire"] = true,
-  ["minecraft:campfire"] = true,
-  ["minecraft:lit_campfire"] = true,
-  ["minecraft:magma_block"] = true,
   ["minecraft:tnt"] = true,
 }
-
-function isHazard(block)
-  if block == nil then
-    return false
-  end
-  return HAZARD_BLOCKS[block]
-end
-
-function isDiggable(block, digAbundant)
-  if block != nil then
-    return not INDESTRUCTIBLE_BLOCKS[block] and not (ABUNDANT_BLOCKS[block] and digAbundant)
-  end
-  return false
-end
 
 function _getBlock(detectFunc, inspectFunc)
   if not detectFunc() then
@@ -53,115 +74,155 @@ function _getBlock(detectFunc, inspectFunc)
       print("getBlock.inspectFunc() successfully returned a nil block")
       return nil
     end
-    return data.name
+    return {
+      name = data.name,
+      hazard = HAZARD_BLOCKS[data.name],
+      abundant = ABUNDANT_BLOCKS[data.name],
+      indestructible = INDESTRUCTIBLE_BLOCKS[data.name]
+    }
   else
     print("CRITICAL: getBlock.inspectFunct() retured error " .. data)
     return false
   end
 end
 
--- Adjacent blocks
-front = nil
-up = nil
-down = nil
-
 function getFront()
-  front = _getBlock(turtle.detect, turtle.inspect)
-  return front
+  return _getBlock(turtle.detect, turtle.inspect)
 end
 
 function getUp()
-  up = _getBlock(turtle.detectUp, turtle.inspectUp)
-  return up
+  return _getBlock(turtle.detectUp, turtle.inspectUp)
 end
 
 function getDown()
-  down = _getBlock(turtle.detectDown, turtle.inspectDown)
-  return down
+  return _getBlock(turtle.detectDown, turtle.inspectDown)
+end
+
+function isHazard(block)
+  return block ~= nil and (block == false or block.hazard)
 end
 
 function inDanger()
-  getFront()
-  getUp()
-  getDown()
-  return isHazard(front) or isHazard(up) or isHazard(down)
+  local block = getFront()
+  if isHazard(block) then
+    print("Hazard in front: " .. front.name)
+    return true
+  end
+  block = getUp()
+  if isHazard(block) then
+    print("Hazard above: " .. up.name)
+    return true
+  end
+  block = getDown()
+  if isHazard(block) then
+    print("Hazard below: " .. down.name)
+    return true
+  end
+  return false
 end
 
-function _dig(block, digFunc, digAbundant)
-  if block != nil and not INDESTRUCTIBLE_BLOCKS[block] and not (ABUNDANT_BLOCKS[block] and digAbundant) then
+function _dig(blockFunc, digFunc, digAbundant)
+  local block = blockFunc()
+  if block == nil then
+    print("_dig found no block, digging anyway.")
+    return digFunc()
+  elseif block == false then
+    print("_dig failed on blockFunc error.")
+  elseif  inventoryFull() then 
+    print("_dig failed, inventory full.")
+  elseif block.indestructible then
+    print("_dig failed, indestructible " .. block.name)
+  elseif block.abundant and not digAbundant then
+    print("_dig failed, abundant " .. block.name)
+  else
+    print("digging.")
     return digFunc()
   end
   return false
 end
 
 function digFront(digAbundant)
-  return _dig(getFront(), turtle.dig, digAbundant)
+  return _dig(getFront, turtle.dig, digAbundant)
 end
 
 function digUp(digAbundant)
-  return _dig(getUp(), turtle.digUp, digAbundant)
+  return _dig(getUp, turtle.digUp, digAbundant)
 end
 
 function digDown(digAbundant)
-  return _dig(getDown(), turtle.digDown, digAbundant)
+  return _dig(getDown, turtle.digDown, digAbundant)
 end
 
--- Position (coordinates relative to starting position).
-position = { x = 0, y = 0, z = 0 }
-
--- Directions: 0 = north, 1 = east, 2 = south, 3 = west, 4 = up, -1 = down
+-- Direction: 0 = north, 1 = east, 2 = south, 3 = west, 4 = up, -1 = down
 direction = 0
 
 -- Turn: -1 = left, 1 = right
 function turn(turnDirection)
-  if turnDirection == 1
+  print("turning " .. turnDirection)
+  if turnDirection == 1 then
     turtle.turnRight()
-  elseif turnDirection == -1
+  elseif turnDirection == -1 then
     turtle.turnLeft()
   else
     return false
   end
   direction = (direction + turnDirection) % 4
-  getFront()
 end
 
-function _move(moveFunc)
-  if moveFunc() then
-    if moveFunc == turtle.forward then
-      if direction == 0 then
-        position.z = position.z - 1
-      elseif direction == 1 then
-        position.x = position.x + 1
-      elseif direction == 2 then
-        position.z = position.z + 1
-      elseif direction == 3 then
-        position.x = position.x - 1
-      end
-    elseif moveFunc == turtle.up then
-      position.y = position.y + 1
-    elseif moveFunc == turtle.down then
-      position.y = position.y - 1
-    else
-      return false
+function turnTo(newDirection)
+  local diff = (newDirection - direction) % 4
+  if diff == 3 then
+    turn(-1)
+  elseif diff == 2 then
+    turn(1); turn(1)
+  elseif diff == 1 then
+    turn(1)
+  end
+end
+
+-- Position (coordinates relative to starting position).
+position = { x = 0, y = 0, z = 0 }
+
+function positionStr()
+  return "(" .. position.x .. "," .. position.y .. "," .. position.z .. ")"
+end
+
+function moveForward()
+  if turtle.forward() then
+    if direction == 0 then
+      position.z = position.z - 1
+    elseif direction == 1 then
+      position.x = position.x + 1
+    elseif direction == 2 then
+      position.z = position.z + 1
+    elseif direction == 3 then
+      position.x = position.x - 1
     end
-    getFront()
-    getUp()
-    getDown()
+    -- print("Moved forward " .. positionStr())
     return true
   end
+  print("turtle.forward() returned false.")
   return false
 end
 
-function move()
-  return _move(turtle.forward)
-end
-
 function moveUp()
-  return _move(turtle.up)
+  if turtle.up() then
+    position.y = position.y + 1
+    -- print("Moved up " .. positionStr())
+    return true
+  end
+  print("turtle.up() returned false.")
+  return false
 end
 
 function moveDown()
-  return _move(turtle.down)
+  if turtle.down() then
+    position.y = position.y - 1
+    -- print("Moved down " .. positionStr())
+    return true
+  end
+  print("turtle.down() returned false.")
+  return false
 end
 
 function refuel()
@@ -182,8 +243,8 @@ end
 fuel_buffer = 300
 function hasFuel()
   local min_fuel = fuel_buffer + math.abs(position.x) + math.abs(position.y) + math.abs(position.z)
-  while turtle.getFuelLevel() <= min_fuel_level then
-    print("Low fuel. " .. fuel_level .. " remaining of " .. min_fuel_level .. " minimum.")
+  while turtle.getFuelLevel() <= min_fuel do
+    print("Low fuel.")
     if not refuel() then
       return false
     end
@@ -191,49 +252,132 @@ function hasFuel()
   return true
 end
 
+move_attempts = 4
 function safeMove(moveFunc, digFunc)
-  while hasFuel() and not inDanger() and not moveFunc() do
-      if not digFunc(true) then
-        return false
-      end
+  if not hasFuel() then
+    print("safeMove failed on fuel")
+    return false
+  end
+  for attempt = 1, move_attempts do
+    if not moveFunc() then
+      digFunc(true)
+    else
+      return true
     end
   end
   return false
 end
 
 function mineShaft(maxDepth)
-  print("Starting fuel " .. turtle.getFuelLevel())
-
+  turnTo(3)
   while position.y > maxDepth do
-    print("Digging down.")
     if not safeMove(moveDown, digDown) then
       return false
     end
+    local turn_direction = 0
     digFront(false)
-    if inDanger() then
-      return false
+    if direction == 3 then
+      turn_direction = 1
+    elseif direction == 1 then
+      turn_direction = -1
     end
-    for turn in 0, 2 do
-      turnLeft()
-      if inDanger() then
-        return false
-      end
+    for turn_num = 1, 2 do
+      turn(turn_direction)
       digFront(false)
-      if inDanger() then
-        return false
-      end
     end
   end
+  print("Reached max depth.")
 end
 
-function goHome()
-  print ("Going home.")
-  while position.y < 0 do
+function goY(targetY)
+  while position.y < targetY do
     if not moveUp() then
       digUp(true)
     end
   end
+  while position.y > targetY do
+    if not moveDown() then
+      digDown(true)
+    end
+  end
+  return true
 end
 
-mineShaft(-10)
+function goZ(targetZ)
+  while position.z > targetZ do
+    turnTo(0)
+    moveForward()
+  end
+  while position.z < targetZ do
+    turnTo(2)
+    moveForward()
+  end
+  return true
+end
+
+function goHome()
+  goY(0)
+  goZ(0)
+  turnTo(2)
+end
+
+shaftStart = {y = 0, z = 0}
+
+function goToShaft()
+  print("Moving to shaft start at " .. shaftStart.y .. ", " .. shaftStart.z)
+  if goZ(shaftStart.z) and goY(shaftStart.y) then
+    turnTo(0)
+    return true
+  end
+  return false
+end
+
+function dumpCargo()
+  print("Going home to dump cargo.")
+  goHome()
+  print("Dumping cargo.")
+  dropInventory()
+end
+
+function main()
+  local shaftDepth = -256
+  while true do
+    if not goToShaft() then
+      goHome()
+      print("goToShaft returned false.")
+      return
+    end
+    mineShaft(shaftDepth)
+    shaftStart.y = position.y + 1
+    if position.y <= shaftDepth then
+      print("Reached max depth of " .. shaftDepth)
+      goY(0)
+      plugShaft()
+      shaftStart.z = shaftStart.z - 2
+      shaftStart.y = 0
+    end
+    local block_below = getDown()
+    if block_below ~= nil and block_below.name == "minecraft:bedrock" then
+      print("Hit bedrock at " .. position.y .. ", starting new shaft.")
+      goY(0)
+      plugShaft()
+      shaftStart.z = shaftStart.z - 2
+      shaftStart.y = 0
+    end
+    if inventoryFull() then
+      dumpCargo()
+    end
+    while not hasFuel() do
+      goHome()
+      print("NEED FUEL")
+    end
+    if shaftStart.z < -6 then
+      print("Mined all shafts!")
+      return
+    end
+  end
+end
+
+main()
 goHome()
+dropInventory()
